8ad1e3128365f44fe6424c3400b3bc4ebf21fecf
unknown <kulangana@yandex.ru git config --global user.name Kulangana-ddgit --global user.email kulangana@yandex.ru>
2022-09-24 13:57:57 +0300

    task 10

---
+++ task10/Program.cs
@@ -0,0 +1,26 @@
+﻿/*Задача 10: Напишите программу, которая принимает на вход трёхзначное число 
+и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1*/
+
+int getSecondDigit(int number)
+{
+    int result = new int();
+    result = number % 100 / 10;
+    return result;
+}
+
+Console.WriteLine("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number > 99 & number < 1000)
+{
+    int num2 = getSecondDigit(number);
+    Console.WriteLine(num2);
+}
+else
+{
+    Console.WriteLine("Число не трехзначное");
+}
