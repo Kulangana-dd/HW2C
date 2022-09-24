e035491c043c8f31121e2c6150d983e061e84863
unknown <kulangana@yandex.ru git config --global user.name Kulangana-ddgit --global user.email kulangana@yandex.ru>
2022-09-24 17:20:47 +0300

    task 15

---
+++ task15/Program.cs
@@ -0,0 +1,16 @@
+﻿/*Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, 
+является ли этот день выходным.*/
+
+Console.WriteLine("Введите день недели от 1 до 7:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number == 6 || number == 7)
+{
+    Console.WriteLine("выходной день");
+}
+else if (number > 0 && number < 6)
+{
+    Console.WriteLine("будний день");
+}
+else
+Console.WriteLine("Такого дня недели не существует");
